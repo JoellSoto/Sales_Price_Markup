6b6e56d44e51ca747cf5024f717f0cae60321654
Joel Soto <joelarlindosoto1@gmail.com>
2023-09-28 21:50:45 +0200

    fix: home page view fixed

--- markup_app/src/Components/PriceList.Component.jsx
+++ markup_app/src/Components/PriceList.Component.jsx
@@ -20,90 +20,90 @@ import {deleteItem} from '../utils/InputsFunctions';
        }
     return (<div className={Styles.totalPricesContainer}>
     <span>
-      {produts.map((item,idx)=>
-          <div className={Styles.priceContainer} key={idx} onClick={()=>handleSelect(item,idx)}>
-          <span className={Styles.pricesTools}>
-          <span className={Styles.prices}>
+      {produts.map((item,idx)=>{
+        if(parseInt(item.user) ===activeUser()){
+          return(
+            <div className={Styles.priceContainer} key={idx} onClick={()=>handleSelect(item,idx)}>
+            <span className={Styles.pricesTools}>
+            <span className={Styles.prices}>
+            <Typography
+            
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:600,
+              fontSize:16
+            }}
+          >
+          Produto
+          </Typography>
           <Typography
           
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:600,
-            fontSize:16
-          }}
-        >
-         Produto
-        </Typography>
-        <Typography
-         
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:600,
-            fontSize:16,
-            marginLeft:1
-          }}
-        >
-         Custo
-        </Typography>
-     
-        <Typography
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:600,
+              fontSize:16,
+              marginLeft:1
+            }}
+          >
+          Custo
+          </Typography>
+      
+          <Typography
+          
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:600,
+              fontSize:16
+            }}
+          >
+          Preço 
+          </Typography>
+              
+          <Typography
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:500,
+              fontSize:13,
+              
+            }}
+          >
+            {item.nome}
+          </Typography>
+          <Typography
+            className={Styles.pricesElement}
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:500,
+              fontSize:13,
+              whiteSpace: 'nowrap'
+
+            }}
+          >
+          {parseInt(item.unityCost)+" Mts"} 
+          </Typography>
+    
+          <Typography
+            className={Styles.pricesElementt}
+            sx={{
+              fontFamily: 'Verdana, Geneva, sans-serif',
+              fontWeight:500,
+              fontSize:13,
+              whiteSpace: 'nowrap'
+    
+            }}
+          >
+          {parseInt(item.recomendedPrice)} Mts
+          </Typography>
         
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:600,
-            fontSize:16
-          }}
-        >
-         Preço 
-        </Typography>
-            
-        <Typography
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:500,
-            fontSize:13,
-            
-          }}
-        >
-          {item.nome}
-        </Typography>
-        <Typography
-          className={Styles.pricesElement}
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:500,
-            fontSize:13,
-            whiteSpace: 'nowrap'
-
-          }}
-        >
-        {parseInt(item.unityCost)+" Mts"} 
-        </Typography>
-  
-        <Typography
-          className={Styles.pricesElementt}
-          sx={{
-            fontFamily: 'Verdana, Geneva, sans-serif',
-            fontWeight:500,
-            fontSize:13,
-            whiteSpace: 'nowrap'
-  
-          }}
-        >
-         {parseInt(item.recomendedPrice)} Mts
-        </Typography>
-       
-       </span>
-        <IconButton onClick={()=>handleDeleteClick(idx)}>
-          <DeleteIcon style={{ color: iconColor }} className={Styles.deleteButton}/>
-        </IconButton>
-       
-       </span>  
-      </div> 
-       
-       
-
-       )} 
+        </span>
+          <IconButton onClick={()=>handleDeleteClick(idx)}>
+            <DeleteIcon style={{ color: iconColor }} className={Styles.deleteButton}/>
+          </IconButton>
+        
+        </span>  
+        </div> 
+        
+       )}})} 
        </span>
   </div>)
     
